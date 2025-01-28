fe77d66d54c72718eb5f442498fa268e390bba59
Chisom Udonsi <udonsichisom02@gmail.com>
2025-01-27 20:52:09 -0500

    chore: remove unnecessary dist directory

--- dist/lib/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGODB_SCHEMA_OPTIONS = exports.PASSWORD_REGEX = void 0;
-exports.PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^\w\s])/;
-exports.MONGODB_SCHEMA_OPTIONS = {
-  timestamps: true,
-  minimize: true,
-  versionKey: false,
-};
